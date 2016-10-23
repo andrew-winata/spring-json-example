168ca9bb2921ccf85248bf9a1513da8a9c6c1ac2
Andrew Winata <andrew.winata@gdn-commerce.com>
2016-10-23 20:59:07 +0700

    refactoring the json

--- spring-json-example/src/main/java/com/example/repository/CountryRepository.java
+++ spring-json-example/src/main/java/com/example/repository/CountryRepository.java
@@ -1,14 +1,12 @@
 package com.example.repository;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.model.Country;
-import com.example.model.JsonCollectionModel;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,13 +17,12 @@ public class CountryRepository {
 	private ObjectMapper objectMapper;
 
 	public List<Country> findAll() throws Exception {
-		List<Country> result = new ArrayList<>();
+		List<Country> result;
 		ClassLoader classLoader = getClass().getClassLoader();
-		JsonCollectionModel<Country> jsonResult = objectMapper.readValue(
+		result = objectMapper.readValue(
 				new File(classLoader.getResource("countries.json").getFile()),
-				new TypeReference<JsonCollectionModel<Country>>() {
+				new TypeReference<List<Country>>() {
 				});
-		result.addAll(jsonResult.getContent());
 		return result;
 	}
 }
